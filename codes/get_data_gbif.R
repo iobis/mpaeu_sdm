e7df0d72e4104a280c1f048002d03413e337b41d
silasprincipe <53846571+silasprincipe@users.noreply.github.com>
2023-06-23 19:30:04 +0200

    Updated to the latest draft version

--- codes/get_data_gbif.R
+++ codes/get_data_gbif.R
@@ -13,7 +13,7 @@ library(obissdm)
 splist <- read.csv("data/gbif_splist_20230623.csv")
 
 splist$full_name <- paste(
-  splist$scientificName, splist$scientificNameAuthorship
+  splist$scientificname, splist$authority
 )
   
 # Check GBIF taxonomic backbone
@@ -28,11 +28,7 @@ non_match <- splist[is.na(check_names$usageKey),]
 
 # Manually correct those
 # Check using only the species name
-check_nm <- name_backbone_checklist(non_match$scientificName,
-                                    phylum = non_match$phylum,
-                                    class = non_match$class,
-                                    family = non_match$family,
-                                    genus = non_match$genus,
+check_nm <- name_backbone_checklist(non_match$scientificname,
                                     strict = T)
 check_nm$taxonID <- non_match$taxonID
 
@@ -47,39 +43,6 @@ splist_match$usageKey[!is.na(splist_match$usageKey.y)] <- splist_match$usageKey.
 splist_match <- select(splist_match, -usageKey.x, -usageKey.y)
 
 non_match <- check_nm[is.na(check_nm$usageKey),]
-
-
-
-# Assign checking using synonim
-check_syn_nm <- lapply(non_match$taxonID, function(x){
-  res <- try(wm_synonyms(x))
-  nv <- NA
-  if (class(res)[1] != "try-error") {
-    if (any(res$status %in% c("alternate representation", "superseded combination"))) {
-      syn <- res$scientificname[res$status %in%  c("alternate representation", "superseded combination")][1]
-      cn <- name_backbone(syn, strict = T)
-      if (cn$matchType != "NONE") {
-        nv <- cn$usageKey[1]
-      }
-    }
-  }
-  Sys.sleep(2)
-  return(nv)
-})
-
-check_syn_nm <- unlist(check_syn_nm)
-
-non_match$usageKey <- check_syn_nm
-
-# Match synonims
-splist_match <- left_join(splist_match, non_match[,c("taxonID", "usageKey")],
-                          by = "taxonID")
-splist_match$usageKey <- splist_match$usageKey.x
-splist_match$usageKey[!is.na(splist_match$usageKey.y)] <- splist_match$usageKey.y[!is.na(splist_match$usageKey.y)]
-
-splist_match <- select(splist_match, -usageKey.x, -usageKey.y, -full_name)
-
-splist_match <- splist_match[!is.na(splist_match$usageKey),]
 
 # Get data from GBIF and save
 mp_get_gbif(sci_names = splist_match$usageKey)
