34e683f700f6252df2111aca782add9d3cbcff06
silasprincipe <53846571+silasprincipe@users.noreply.github.com>
2024-09-17 13:05:21 +0200

    Update post_habitat.R

--- codes/post_habitat.R
+++ codes/post_habitat.R
@@ -207,7 +207,7 @@ join_predictions <- function(species, model_acro, hab_name,
     return(invisible(NULL))
 }
 
-for (k in biogenic_groups) {
+for (k in 1:length(biogenic_groups)) {
     tg_biog <- names(biogenic_groups)[k]
 
     join_predictions(biogenic_groups[[k]], model_acro = acro, hab_name = tg_biog)
